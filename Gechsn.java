2c50eb9eb0797f0b6a1c88f2497332e103669d90
Sahana M M <159983911+sahana-mm@users.noreply.github.com>
2024-04-06 21:17:19 +0530

    Update Gechsn.java

--- Gechsn.java
+++ Gechsn.java
@@ -1,7 +1,16 @@
-public class Gechsn{
+*To add two number and store in a another variable.
+  public class Gechsn{
   public static void main(String[] args){
     int a=1,b=2;
     int sum=a+b;
     System.out.println(sum);
   }
 }
+
+*to subtract two numbers and store it in another variable.
+  public class Gechsn{
+  public static void main(String[] args){
+    int a=30,b=40;
+    int sum=a-b;
+    System.out.println(sum);
+  }
