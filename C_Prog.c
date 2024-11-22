c06405856a01b0dd5fb55a0c3b3ed1cc6a276cb2
Sahana M M <159983911+sahana-mm@users.noreply.github.com>
2024-11-22 20:33:28 +0530

    Update C_Prog.c

--- C_Prog.c
+++ C_Prog.c
@@ -1,13 +1,6 @@
 Today I start to write anything I learnt  about C programming here and I'm preparing for TCS NQT nad I have to clear it by taking it in an one chanse.
+  
 1).To print the matrix which is after rotated for 90 degrees(by transposing and interchanging the rows);
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 // Function to transpose the matrix
